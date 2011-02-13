c64436ab178c666ae109311ef333b83debfc0376
Brandyn Brosemer <bbrosemer@me.com>
2011-02-12 20:29:53 -0500

    Test

--- RootViewController.h
+++ RootViewController.h
@@ -2,6 +2,7 @@
 #import <CoreData/CoreData.h>
 #import "ChatController.h"
 //@class ChatController;
+// This is a test to see how version control works in xcode 4.0
 
 @protocol RootDelegate <NSObject>
 @optional
